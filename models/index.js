6ee0baf058450da419b369051682e725681fdcd4
Kelly Kim <kellykim408@gmail.com>
2020-10-26 21:20:56 -0700

    Update comments on index

--- models/index.js
+++ models/index.js
@@ -1,35 +1,53 @@
 'use strict';
 
-var fs        = require('fs');
-var path      = require('path');
+// Import node.js. This allows the user to use the file system 
+var fs = require('fs');
+// Import node.js path module. This lets the user to to connect with the file & directory path
+var path = require('path');
+// Import sequelize module. This lets the user connect with the server and a database. Which sequelize does through object syntax.
 var Sequelize = require('sequelize');
-var basename  = path.basename(module.filename);
-var env       = process.env.NODE_ENV || 'development';
-var config    = require(__dirname + '/../config/config.json')[env];
-var db        = {};
+// This module.filename returns the absolute path of the file where it's called. The path.basename shortens it, but equals index.js
+var basename = path.basename(module.filename);
+// This env is assigned to 'development' if the NODE_ENV key in process.env is not defined
+var env = process.env.NODE_ENV || 'development';
+// Import the value corresponding to the env key in the config.json file. This allows login configuration to access MySQL
+var config = require(__dirname + '/../config/config.json')[env];
+// Assign the variable db to it an empty object
+var db = {};
 
+// If use_env_variable is a key in the config object
 if (config.use_env_variable) {
+  // Then we create a new sequelize using the defined env_variable
   var sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
+  // If its not, then crete a new sequelize using the config values listed
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
 fs
+  // ReaddirSync returns an array containing files from the current directory (__dirname)
   .readdirSync(__dirname)
-  .filter(function(file) {
+  // The .filter uses callback function on each array. A new array is what the filter method returns 
+  .filter(function (file) {
+    // Returns true if the file does not begin with '.', & the filename does not equal 'index.js' & it must be a javascript file 
     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
   })
-  .forEach(function(file) {
+  // The .forEach passes each array item into a callback function
+  .forEach(function (file) {
     var model = sequelize['import'](path.join(__dirname, file));
+    //  the db object adds a new key pair value. The key is the contructor's name & the value is the class constructor model
     db[model.name] = model;
   });
 
-Object.keys(db).forEach(function(modelName) {
+// Object.keys is the db object returning an array containing keys. The forEach passes each array item to a callback function
+Object.keys(db).forEach(function (modelName) {
+  // Because the forEach loops through an array of db's keys, each modelName is a key in db. If associate function is defined, models object is the argument
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
 });
 
+// db.sequelize = sequelize and Sequelize
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
